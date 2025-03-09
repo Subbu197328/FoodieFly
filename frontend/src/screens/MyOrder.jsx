501d0a6da475c6aa53ae729d024cee46e28a6fe8
Balasubramanyam Singh <123584080+Subbu197328@users.noreply.github.com>
2025-03-09 19:13:37 +0530

    Update MyOrder.jsx

--- frontend/src/screens/MyOrder.jsx
+++ frontend/src/screens/MyOrder.jsx
@@ -10,7 +10,7 @@ export default function MyOrder() {
         if (!email) return;
 
         try {
-            const res = await fetch("http://localhost:5000/api/auth/myOrderData", {
+            const res = await fetch("https://foodiefly.onrender.com/api/auth/myOrderData", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email }),
